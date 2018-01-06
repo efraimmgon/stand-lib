5207e791bf011fd74a8ac6199a2858fbc2d23557
efraimmgon <efraimmaugusto@gmail.com>
2018-01-06 16:37:06 -0300

    remove unnecessary handlers and subs;

--- src/stand_lib/handlers.cljs
+++ src/stand_lib/handlers.cljs
@@ -4,6 +4,9 @@
    [stand-lib.re-frame.utils :refer [query]]
    [stand-lib.utils :refer [extract-ns+name]]))
 
+(defn query-sub [db [event ns+name]]
+  (get-in db (extract-ns+name ns+name)))
+
 ; ------------------------------------------------------------------------------
 ; Event handlers
 ; ------------------------------------------------------------------------------
@@ -20,8 +23,18 @@
  (fn [db [_ ns+name val]]
    (assoc-in db (extract-ns+name ns+name) val)))
 
+(rf/reg-event-db
+ :set-state
+ (fn [db [_ ks val]]
+   (assoc-in db ks val)))
+
+(rf/reg-event-db
+ :update-state
+ (fn [db [_ ks f]]
+   (update-in db ks f)))
+
 ; ------------------------------------------------------------------------------
 ; Subs
 ; ------------------------------------------------------------------------------
 
-(rf/reg-sub :error query)
+(rf/reg-sub :query query-sub)
